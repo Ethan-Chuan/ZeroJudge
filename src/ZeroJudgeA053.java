8eccc48c089a3f3c96e1fbd40c71c1fa19ede66a
Ethan <tethance6203@gmail.com>
2018-05-02 18:46:01 +0800

    Completed Problem A053
    Different number of solved questions get different scores.
    Using lookup table.

--- src/ZeroJudgeA053.java
+++ src/ZeroJudgeA053.java
@@ -7,31 +7,16 @@ import java.util.Scanner;
  */
 public class ZeroJudgeA053 {
     public static void main(String[] args) {
-        int[] stepNumQ = new int[]{40, 20, 10, -1};
+        int[] stepScore = new int[]{0, 1, 2, 6, 0};
+        int[] baseScore = new int[]{100, 80, 60, 0, 0};
+        int[] stepNumQ = new int[]{40, 20, 10, 0, 0};
         Scanner scanner = new Scanner(System.in);
         
         while(scanner.hasNext()){
             int n = scanner.nextInt();
             int ret, i;
             for(i=0; i<4  && n<=stepNumQ[i]; i++);
-            switch(i){
-                case 0:
-                    ret = 100;
-                    break;
-                case 1:
-                    ret = 80;
-                    ret += (n-stepNumQ[i])*1;
-                    break;
-                case 2:
-                    ret = 60;
-                    ret += (n-stepNumQ[i])*2;
-                    break;
-                case 3:
-                    ret = n*6;
-                    break;
-                default:
-                    ret = 0;
-            }
+            ret = baseScore[i] + stepScore[i]*(n-stepNumQ[i]);
             System.out.println(ret);
         }
     }
