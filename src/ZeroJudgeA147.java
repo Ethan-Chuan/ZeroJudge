a393071c50b323ea6879f2799d825db84d07102d
Ethan <tethance6203@gmail.com>
2018-05-04 16:24:52 +0800

    Completed Problem A147
    Skip number of multiply by 7.
    Also using Java 8 syntax implement.

--- src/ZeroJudgeA147.java
+++ src/ZeroJudgeA147.java
@@ -1,23 +1,33 @@
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 /**
  *
  * @author Ethan
  */
 public class ZeroJudgeA147 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x;
         
+        while( (x = scanner.nextInt()) !=0 ){
+            printExceptNumOfMultiplyBy7(x);
+            System.out.println();
+        }
     }
     
     // skip number of multiply by 7.
     static void printExceptNumOfMultiplyBy7(int x){
-        int last;
-        int q = x / 7;
-        int r = x % 7;
-         
-        // find last number divisible by 7.
-        if(q==0)
-            last = 0;
-        else
-            last = q*7;
-        
+        for(int i=1; i<x; i++){
+            if(i%7==0)
+                continue;
+            System.out.print(i + " ");
+        }
+    }
+    
+    // Java 8
+    static void printExceptNumOfMultiplyBy7(int x, int y){
+        IntStream.range(1, x).filter(i -> (i % 7) != 0).forEach(i -> System.out.print(i + " "));
     }
 }
